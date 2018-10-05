5a78700ab9b27580af71ff3d1141fe07c78731e2
Vitali Shcharbin <vitali.shcharbin@gmail.com>
2018-10-04 22:11:26 -0700

    Correct listenAddress configuration.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
+    listenAddress: '127.0.0.1',
     hostname: '127.0.0.1',
     port: 9876,
     colors: true,
